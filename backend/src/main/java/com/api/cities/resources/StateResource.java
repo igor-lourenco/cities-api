fc03ae66dde58329d7fceef9daf295016ced1087
igor-lourenco <igor.lourencosantos@gmail.com>
2021-10-26 12:35:41 -0300

    Find by Id State

--- backend/src/main/java/com/api/cities/resources/StateResource.java
+++ backend/src/main/java/com/api/cities/resources/StateResource.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,10 @@ public class StateResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<StateDTO> findById(@PathVariable Long id){
+		
+		StateDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
 }
--- backend/src/main/java/com/api/cities/services/StateService.java
+++ backend/src/main/java/com/api/cities/services/StateService.java
@@ -1,5 +1,7 @@
 package com.api.cities.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.api.cities.dto.StateDTO;
 import com.api.cities.entities.State;
 import com.api.cities.repositories.StateRepository;
+import com.api.cities.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class StateService {
@@ -22,5 +25,10 @@ public class StateService {
 		return entity.map(x -> new StateDTO(x, x.getDdd()));
 	}
 	
-	
+	@Transactional()
+	public StateDTO findById(Long id) {
+		Optional<State> entity = repository.findById(id);
+		State obj = entity.orElseThrow(() ->  new ResourceNotFoundException(" Id não existe"));
+		return new StateDTO(obj, obj.getDdd());
+	}
 }
